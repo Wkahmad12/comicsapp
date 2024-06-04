b362b70176586fbb77f23c87332ddab9119baf50
Wkahmad12 <114064137+Wkahmad12@users.noreply.github.com>
2024-06-04 16:57:48 +0400

    changing

--- app/src/main/java/com/example/comicsapp/splashcreen.kt
+++ app/src/main/java/com/example/comicsapp/splashcreen.kt
@@ -1,18 +1,28 @@
 package com.example.comicsapp
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.VideoView
 
 class splashcreen : AppCompatActivity() {
-    private val SPLASH_TIME_OUT: Long = 2000
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashcreen)
-        android.os.Handler().postDelayed({
-            startActivity(Intent(this, login::class.java))
-            finish()
-        }, SPLASH_TIME_OUT)
 
+        val videoView = findViewById<VideoView>(R.id.video)
+        val videoPath = "android.resource://" + packageName + "/" + R.raw.marvel
+
+        val videoURI = Uri.parse(videoPath)
+        videoView.setVideoURI(videoURI)
+        videoView.start()
+
+        videoView.setOnCompletionListener {
+            val intent = Intent(this, login::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
